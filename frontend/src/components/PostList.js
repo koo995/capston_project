0f681c20e355712d1bc1f963fcfe0f306a053a3d
gunhong951 <gunhong951@gmail.com>
2023-03-19 02:46:51 +0900

    리액트 라우팅 페이지 간단히 처리

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Post from "./Post";
+// import { Alert } from "antd";
 
 const apiurl = "http://localhost:8000/api/posts";
 
@@ -19,11 +20,13 @@ function PostList() {
 
   return (
     <div>
-      <h2>PostList</h2>
-      {postList.map((post) => (
-        // key할당했는데 왜 계속 경고가 뜨지?
-        <Post post={post} key={post.id} />
-      ))}
+      {/* {postList.length === 0 && (
+        // <Alert type="warning" message="포스팅이 없습니다" />
+      )} */}
+      {/* postList에서 꺼내온 녀석은 object이니까 render가 안된다? JSON.stringify을 이용한다 */}
+      {postList.map((post) => {
+        return <Post post={post} key={post.id} />;
+      })}
     </div>
   );
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import PostList from "components/PostList";
+import { Route } from "react-router-dom";
+import AppLayout from "components/Layout/AppLayout";
+import Home from "./Home";
+import accountRoutes from "./account";
 
 function Root() {
   return (
-    <div>
-      최상위 컴포넌트
-      <PostList />
-    </div>
+    <AppLayout>
+      <Route exact path="/" component={Home} />
+      {/* 여기서 exact을 썼더니 login profile url에 의한 페이지가 제대로 나오지 않았어*/}
+      <Route path="/account" component={accountRoutes} />{" "}
+    </AppLayout>
   );
 }
 
