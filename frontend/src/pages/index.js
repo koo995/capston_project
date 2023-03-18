c9ef1f22b723bf2d37d7309ae706cf18ee73f5cc
gunhong951 <gunhong951@gmail.com>
2023-03-18 20:55:20 +0900

    react 페이지기본 구성

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "components/PostList";
+
+function Root() {
+  return (
+    <div>
+      최상위 컴포넌트
+      <PostList />
+    </div>
+  );
+}
+
+export default Root;
