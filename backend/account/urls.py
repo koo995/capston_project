eeb5b66cbb9f85cf5b002ee29cd791a1b9dcf9bf
gunhong951 <gunhong951@gmail.com>
2023-03-23 00:55:39 +0900

    회원가입 구현 완료

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -4,10 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 
 urlpatterns = [
-    path("identicon/image/<path:data>/", pydenticon_image, name="pydenticon_image"),
+    path("identicon/image/<path:data>.png", pydenticon_image, name="pydenticon_image"),
+    path("signup/", views.SignupView.as_view(), name="login"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- frontend/src/components/Layout/AccountSideBar.js
+++ frontend/src/components/Layout/AccountSideBar.js
@@ -4,14 +4,14 @@ import { Menu } from "antd";
 function AccountSideBar() {
   return (
     <div>
-      <Menu
+      {/* <Menu
         defaultSelectedKeys={["1"]}
         defaultOpenKeys={["sub1"]}
         mode="horizontal inline"
       >
         <Menu.Item>Avatar</Menu.Item>
         <Menu.Item>로그아웃</Menu.Item>
-      </Menu>
+      </Menu> */}
     </div>
   );
 }
