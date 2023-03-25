b739cf19272edecaf7a5e1ec194536746ac1d955
gunhong951 <gunhong951@gmail.com>
2023-03-25 18:29:46 +0900

    postDetail절반쯤? 로그인된 회원 프로필까지 띄우기왼료

--- frontend/src/components/Layout/AccountSideBar.js
+++ frontend/src/components/Layout/AccountSideBar.js
@@ -1,44 +1,81 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { Avatar, Button } from "antd";
 import { useHistory } from "react-router-dom";
-import { useAppContext } from "store";
-import { deleteToken } from "store";
+import { useAppContext, deleteToken } from "store";
+import Axios from "axios";
 
 function AccountSideBar() {
   const history = useHistory();
   const { store, dispatch } = useAppContext();
-  const { isAuthenticated } = store;
+  const { jwtAccessToken, isAuthenticated } = store;
+  const [userInfo, setUserInfo] = useState();
 
+  useEffect(() => {
+    async function fetchUserInfo() {
+      const apiUrl = "http://localhost:8000/account/user-info/";
+      const headers = { Authorization: `Bearer ${jwtAccessToken}` };
+      try {
+        const { data: userInfoData } = await Axios.get(apiUrl, { headers });
+        setUserInfo(userInfoData);
+        console.log("userInfoData: ", userInfoData);
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    }
+    fetchUserInfo();
+  }, [jwtAccessToken]); //우와... 여기를 jwtAccessToken 넣어주니 내가 원하는대로 잘 작동하네!!
+  console.log("userInfo: ", userInfo);
   return (
     <>
       {isAuthenticated ? ( //여기를 소괄호로 감싸주는 이유...
-        <Button
-          danger
-          onClick={() => {
-            dispatch(deleteToken());
-          }}
-        >
-          로그아웃
-        </Button>
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: "20px" }}>
+            <Avatar
+              size={"large"}
+              icon={
+                userInfo && (
+                  <img
+                    src={"http://localhost:8000" + userInfo.avatar_url}
+                    alt={"아바타"}
+                  />
+                )
+              }
+            />
+          </div>
+          <div>
+            <Button
+              danger
+              onClick={() => {
+                dispatch(deleteToken());
+              }}
+            >
+              로그아웃
+            </Button>
+          </div>
+        </div>
       ) : (
-        <>
-          <Button
-            type="primary"
-            onClick={() => {
-              history.push("/account/login");
-            }}
-          >
-            로그인
-          </Button>
-          <Button
-            danger
-            onClick={() => {
-              history.push("/account/signup");
-            }}
-          >
-            회원가입
-          </Button>
-        </>
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: "10px" }}>
+            <Button
+              type="primary"
+              onClick={() => {
+                history.push("/account/login");
+              }}
+            >
+              로그인
+            </Button>
+          </div>
+          <div>
+            <Button
+              danger
+              onClick={() => {
+                history.push("/account/signup");
+              }}
+            >
+              회원가입
+            </Button>
+          </div>
+        </div>
       )}
     </>
   );
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { Card, Avatar } from "antd";
-import { EditOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 import "./Post.scss";
 
 function Post({ post }) {
   const { Meta } = Card;
-
+  const history = useHistory();
   const { author, title, content, photo, caption } = post;
   const { avatar_url, nick_name } = author;
+
+  const handleCardClick = () => {
+    history.push(`/posts/${post.id}`); // Replace 'post.id' with the actual post ID property
+  };
+
   return (
     <Card
       hoverable
+      onClick={handleCardClick}
       style={{
-        width: 300,
+        width: 320,
+        height: 400,
         border: "2px solid Black",
       }}
       className="post-card"
-      cover={<img alt="example" src={photo} style={{ width: "95%" }} />}
+      cover={
+        <img
+          alt="example"
+          src={photo}
+          style={{ width: "300px", height: "150px", margin: "5px auto" }}
+        />
+      }
       // actions={[<EditOutlined key="edit" />]}
     >
       <Meta
@@ -28,7 +41,7 @@ function Post({ post }) {
           />
         }
         title={title}
-        description={content}
+        description={<div className="long-content">{content}</div>}
       />
       <hr />
       <div>{caption}</div>
