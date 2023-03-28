10c235398ebccd87a144a915da5a7ff67595d84e
gunhong951 <gunhong951@gmail.com>
2023-03-28 16:50:38 +0900

    새 질문글 올리기까지 완료

---
+++ frontend/src/components/PostNewForm.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { Card, Form, Input, Button, Upload, Modal } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import Axios from "axios";
+import { useAppContext } from "store";
+import { useHistory } from "react-router-dom";
+
+export default function PostNewForm() {
+  const {
+    store: { jwtAccessToken },
+  } = useAppContext();
+  const history = useHistory();
+  const [file, setFile] = useState("");
+  const handleUploadChange = ({ file }) => {
+    setFile(file);
+  };
+
+  const handleFinish = async (fieldValues) => {
+    const { title, content, caption, photo } = fieldValues;
+    const formData = new FormData(); //이건 단순히 자바스크립트 문법이라네? 이름 그대로로 생각하면 되나?
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("caption", caption);
+    if (photo?.file) {
+      formData.append("photo", photo.file);
+    }
+
+    const headers = { Authorization: `Bearer ${jwtAccessToken}` };
+    try {
+      const response = await Axios.post(
+        "http://127.0.0.1:8000/api/posts/",
+        formData,
+        {
+          headers,
+        }
+      );
+      console.log(response);
+      history.push("/");
+    } catch (error) {
+      console.log(error); //이것을 통해 어떤 에러가 있었는지 자세히 알 수 있다.
+    }
+  };
+
+  return (
+    <Form
+      labelCol={{ span: 8 }} //부트스트랩은 한 행이 12 컬럼인데 antd는 24컬럼임
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      onFinish={handleFinish}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="제목"
+        name="title"
+        rules={[{ required: true, message: "제목을 입력해 주세요" }]} //rules을 통해 유효성검사로직이 들어가 잇다
+      >
+        <Input />
+        {/* antd에서 여러줄로 입력할수 있게 TextArea같은 것을 지원해 준다 */}
+      </Form.Item>
+      <Form.Item
+        label="내용"
+        name="content"
+        rules={[{ required: true, message: "내용을 입력해 주세요" }]} //rules을 통해 유효성검사로직이 들어가 잇다
+      >
+        <Input.TextArea />
+      </Form.Item>
+
+      <Form.Item
+        label="태그"
+        name="caption"
+        rules={[{ required: true, message: "태그를 입력해 주세요" }]} //rules을 통해 유효성검사로직이 들어가 잇다
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="사진" name="photo">
+        <Upload
+          listType="picture-card"
+          beforeUpload={() => {
+            return false;
+          }}
+          onChange={handleUploadChange}
+        >
+          {file ? null : ( //여기에 왜 중괄호를 써야하지?
+            //The expression inside the braces will be evaluated, and the result will be rendered.
+            //그냥 쓰면 문자그대로 렌더링될꺼야
+            <div>
+              <PlusOutlined />
+              <div className="ant-upload-text">Upload</div>
+            </div>
+          )}
+        </Upload>
+      </Form.Item>
+      {/* //8칸 이동하고 16칸을 쓰겠다 */}
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <hr />
+      </Form.Item>
+    </Form>
+  );
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,10 +2,35 @@ import PostList from "components/PostList";
 import React from "react";
 import AppLayout from "components/Layout/AppLayout";
 import Tagbar from "components/Layout/Tagbar";
+import { Button } from "antd";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/posts/new");
+  };
+  const postNewButton = //html element을 꼭 return안에서만 만들어야 하는것은 아니고 여기서 이렇게 만들고 전달도 가능하다
+    (
+      <>
+        <Button
+          type="primary"
+          block
+          style={{ marginBottom: "1rem" }}
+          onClick={handleClick}
+        >
+          질문하기
+        </Button>
+      </>
+    );
   //이 부분 처리하는데 꽤나 했갈렸다... 컴포넌트형식으로 전달해야 되는구나
-  return <AppLayout tagbar={<Tagbar />} children={<PostList />} />;
+  return (
+    <AppLayout
+      tagbar={<Tagbar />}
+      children={<PostList />}
+      postNewButton={postNewButton}
+    />
+  );
 }
 
 export default Home;
